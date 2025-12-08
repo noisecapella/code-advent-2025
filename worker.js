6a670072d1d3be4e94dc804af95015d9b585581e
George Schneeloch <noisecapella@gmail.com>
2025-12-08 01:22:39 -0500

    remove unused

--- worker.js
+++
@@ -1,8 +0,0 @@
-import { run_worker } from './pkg';
-
-console.log("worker.js loaded", self)
-
-addEventListener("message", function(event) {
-    console.log("received", event)
-    run_worker(event, postMessage);
-})
